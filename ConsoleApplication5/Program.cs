e6e106a2a25c3c0c155451ffcf6f676e0cf35d9b
h0rror <spiceace@gmail.com>
2016-05-13 22:12:09 -0400

    firstcommit

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* This is connected to github
+ *
+ * return a string that will print Hello World
+ * 
+ * 
+ *  Made public so it is accessible in code/github. 
+ *  
+ * Calling HelloWorldToConsole in the main */
+
+
+namespace COMP123_S2016_Lesson1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldToConsole();
+
+        }
+
+        /**
+        * Prints a HelloWorld message to the console and returns the same message
+        * 
+        * @method HelloWorldToConsole
+        * @return {string} Returns the displayed message
+        * */
+        public static string HelloWorldToConsole()
+        {
+            string HelloWorldString = "Hello, World!";
+            Console.WriteLine(HelloWorldString);
+            return HelloWorldString;
+
+
+        }
+    }
+
+}
